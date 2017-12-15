9dd22d5e3223774d8794ce79cf90addf72bcd872
Calvin Liang <cliangdev@gmail.com>
2017-12-15 11:04:24 -0800

    remove fe code

--- app/src/config/ApiConfig.js
+++
@@ -1,5 +0,0 @@
-const apiVersion = process.env.REACT_APP_API_VERSION
-const protocol = window && window.location && window.location.protocol
-const serverURL = process.env.REACT_APP_API_SERVER_URL || window && window.location && window.location.hostname
-
-export const API_ROOT = `${protocol}//${serverURL}/api/${apiVersion}`
